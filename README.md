ccd26b3dd1977971a54892dc9d42ee1fad36f9ff
Edward Sobczak <sobczak.eddie@gmail.com>
2025-01-21 13:05:29 -0800

    created a second bash script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 2025-01-21-git_conflict
+
+# here we are intentionally creating a conflice like always
--- script.sh
+++ script.sh
@@ -1,3 +1,9 @@
+# this is a bash script file
+# run it like:
+# bash schipt.sh
+
+# git switch -c <NAME>
+echo "my second branch"
 pwd
 echo "hello"
 ls
